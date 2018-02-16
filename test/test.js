d46df23e1c40bfb20980e545a427540f6d2bb9ee
Simona Domnisoru <simona.domnisoru@gmail.com>
2018-02-16 14:00:35 +0100

    Integrate coverage

--- test/test.js
+++ test/test.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
-import { mount, render, shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Star from '../src/Star';
 
